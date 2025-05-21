89d0bb15b635f9b83ed4ad27453faa5e64115d40
eshakhan29 <eshakhan29@gmail.com>
2025-05-22 01:29:21 +0600

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 import './globals.css'
 
 import Footer from '@/components/Footer/Footer'
@@ -8,35 +8,35 @@ import { Fira_Code } from 'next/font/google'
 
 const firaCode = Fira_Code({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
 
-// const title = 'John Doe | Full-Stack Web Developer in Chicago'
+const title = 'Md Eshak Khan | Full-Stack Web Developer in Chicago'
 
-// const description =
-//   "Skilled full-stack web developer in Chicago. I build responsive, user-friendly websites with React, NextJS, and NodeJS. Let's bring your vision to life. Hire me today!"
+const description =
+  "Skilled full-stack web developer in Chicago. I build responsive, user-friendly websites with React, NextJS, and NodeJS. Let's bring your vision to life. Hire me today!"
 
-// const url = process.env.NEXT_PUBLIC_SITE_URL
+const url = process.env.NEXT_PUBLIC_SITE_URL
 
-// export const metadata: Metadata = {
-//   title,
-//   description,
-//   category: 'technology',
-//   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
-//   alternates: {
-//     canonical: url,
-//   },
-//   openGraph: {
-//     title,
-//     description,
-//     url,
-//     siteName: 'John Doe Portfolio',
-//     type: 'website',
-//   },
-//   twitter: {
-//     title,
-//     description,
-//     card: 'summary_large_image',
-//     creator: '@Basit_Miyanji',
-//   },
-// }
+export const metadata: Metadata = {
+  title,
+  description,
+  category: 'technology',
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
+  alternates: {
+    canonical: url,
+  },
+  openGraph: {
+    title,
+    description,
+    url,
+    siteName: 'Md Eshak Khan Portfolio',
+    type: 'website',
+  },
+  twitter: {
+    title,
+    description,
+    card: 'summary_large_image',
+    creator: '@Basit_Miyanji',
+  },
+}
 
 export default function RootLayout({
   children,
